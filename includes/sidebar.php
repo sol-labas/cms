7c67cd557f43b697f4b2f0d15c300008d2e27d19
Angela Kuznetsova <solival@gmail.com>
2019-11-14 19:19:07 +1300

    section12.12

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,16 +1,25 @@
 <div class="col-md-4">
+<?php
+    if(isset($_POST['submit'])){
 
+        $search = $_POST['search'];
+    }
+
+?>
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <div class="input-group">
-            <input type="text" class="form-control">
-            <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+        <form action="">
+
+            <div class="input-group">
+                <input name="search" type="text" class="form-control">
+                <span class="input-group-btn">
+                            <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
-        </div>
+            </div>
+        </form>
         <!-- /.input-group -->
     </div>
 
@@ -51,7 +60,8 @@
     <!-- Side Widget Well -->
     <div class="well">
         <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus
+            laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
     </div>
 
 </div>
