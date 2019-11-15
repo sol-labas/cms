7f245577588e820e5c6374447ca90e745a4af0e1
Angela Kuznetsova <solival@gmail.com>
2019-11-15 14:26:21 +1300

    section13.1

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,8 +23,11 @@
 
                     echo "<li><a href='#'>{$cat_title}</a> </li>";
                 }
-
                 ?>
+                <li>
+                    <a href="admin">Admin</a>
+                </li>
+
             </ul>
         </div>
         <!-- /.navbar-collapse -->
