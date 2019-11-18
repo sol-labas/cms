35bbab4a52b782d20d4857be1e78ec529b1e0d42
Angela Kuznetsova <solival@gmail.com>
2019-11-18 15:06:47 +1300

    section13.6

--- admin/categories.php
+++ admin/categories.php
@@ -19,7 +19,24 @@ include_once "includes/admin_header.php";
                         <small>Authors</small>
                     </h1>
                     <div class="col-xs-6">
-                        <form action="">
+                        <?php
+                        if(isset($_POST['submit'])){
+                            $cat_title = $_POST['cat_title'];
+                            if ($cat_title==''||empty($cat_title)){
+                                echo "Fill the field";
+                            }else{
+                                $query = "INSERT INTO category(cat_title) VALUE ('$cat_title')";
+                                $create_cat = mysqli_query($conn, $query);
+                                if(!$create_cat){
+                                    die("Query failed") . mysqli_error($conn);
+                                }
+
+                            }
+                        }
+
+                        ?>
+
+                        <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat_title">Add Category</label>
                                 <input class="form-control" type="text" name="cat_title">
