11214c261af244c5ba2dc54ec3cf5613fc61e1d9
Angela Kuznetsova <solival@gmail.com>
2019-11-19 19:49:58 +1300

    section13.12

--- admin/categories.php
+++ admin/categories.php
@@ -43,10 +43,6 @@ include_once "includes/admin_header.php";
 
                     </div><!--Add Category Form-->
                     <div class="col-xs-6">
-                        <?php
-
-                        ?>
-
 
                         <table class="table table-bordered table-hover">
                             <thread>
@@ -58,30 +54,8 @@ include_once "includes/admin_header.php";
                             <tbody>
                             <tr>
                                 <?php
-
-                                $query = "SELECT * FROM category";
-                                $select_cat = mysqli_query($conn, $query);
-                                while ($row = mysqli_fetch_assoc($select_cat)) {
-                                    $cat_id = (int)$row['cat_id'];
-                                    $cat_title = $row['cat_title'];
-
-                                    echo "<tr>";
-                                    echo "<td>{$cat_id}</td>";
-                                    echo "<td>{$cat_title}</td>";
-                                    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                                    echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                                    echo "</tr>";
-                                }
-                                ?>
-
-
-                                <?php
-                                if (isset($_GET['delete'])) {
-                                    $the_cat_id = $_GET['delete'];
-                                    $query = "DELETE FROM category WHERE cat_id = {$the_cat_id}";
-                                    $delete_cat = mysqli_query($conn, $query);
-                                    header("location: categories.php");
-                                }
+                                select_all_cat();
+                                delete_cat();
                                 ?>
                             </tr>
                             </tbody>
--- admin/function.php
+++ admin/function.php
@@ -18,4 +18,29 @@ function insert_categories()
     }
 }
 
+function select_all_cat(){
+    global $conn;
+    $query = "SELECT * FROM category";
+    $select_cat = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_assoc($select_cat)) {
+        $cat_id = (int)$row['cat_id'];
+        $cat_title = $row['cat_title'];
 
+        echo "<tr>";
+        echo "<td>{$cat_id}</td>";
+        echo "<td>{$cat_title}</td>";
+        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+        echo "</tr>";
+    }
+}
+
+function delete_cat(){
+    global $conn;
+    if (isset($_GET['delete'])) {
+        $the_cat_id = $_GET['delete'];
+        $query = "DELETE FROM category WHERE cat_id = {$the_cat_id}";
+        $delete_cat = mysqli_query($conn, $query);
+        header("location: categories.php");
+    }
+}
