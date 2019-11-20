04a7bdac6e30f57aa956911dc69fe5699180c3b4
Angela Kuznetsova <solival@gmail.com>
2019-11-20 22:29:43 +1300

    section14.1

---
+++ admin/posts.php
@@ -0,0 +1,67 @@
+<?php
+include_once "includes/admin_header.php";
+?>
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php
+    include_once "includes/admin_navigation.php";
+    ?>
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome to Admin
+                        <small>Authors</small>
+                    </h1>
+
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comments</th>
+                            <th>Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        </tbody>
+
+                    </table>
+
+
+
+
+                    </div>
+
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+        <?php
+        include_once "includes/admin_footer.php";
+        ?>
