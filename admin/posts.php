827b31f9c1dd47da049e2cbc3a0fee659eb09455
Angela Kuznetsova <solival@gmail.com>
2019-11-21 14:03:21 +1300

    section14.2

--- admin/posts.php
+++ admin/posts.php
@@ -34,17 +34,38 @@ include_once "includes/admin_header.php";
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                        <?php
+                        $query = "SELECT * FROM post";
+                        $select_post = mysqli_query($conn, $query);
+                        while ($row = mysqli_fetch_assoc($select_post)) {
+                            $post_id = (int)$row['post_id'];
+                            $post_author = $row['post_author'];
+                            $post_title = $row['post_title'];
+                            $post_category = $row['post_category_id'];
+                            $post_status = $row['post_status'];
+                            $post_image = $row['post_image'];
+                            $post_tags = $row['post_tags'];
+                            $post_comments = $row['post_comm_count'];
+                            $post_date = $row['post_date'];
+
+
+                            echo "<tr>";
+                            echo "<td>{$post_id}</td>";
+                            echo "<td>{$post_author}</td>";
+                            echo "<td>{$post_title}</td>";
+                            echo "<td>{$post_category}</td>";
+                            echo "<td>{$post_status}</td>";
+                            echo "<td><img class='img-responsive' src='../$post_image'></td>";
+                            echo "<td>{$post_tags}</td>";
+                            echo "<td>{$post_comments}</td>";
+                            echo "<td>{$post_date}</td>";
+                            echo "<td><a href='categories.php?delete={$post_id}'>Delete</a></td>";
+                            echo "<td><a href='categories.php?edit={$post_id}'>Edit</a></td>";
+                            echo "</tr>";
+                        }
+
+                        ?>
+
                         </tbody>
 
                     </table>
