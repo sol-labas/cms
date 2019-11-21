dbb7096a010f5e2ef9a2d7fdac4b937f9ab1999f
Angela Kuznetsova <solival@gmail.com>
2019-11-21 20:14:27 +1300

    section14.3

--- admin/posts.php
+++ admin/posts.php
@@ -18,71 +18,44 @@ include_once "includes/admin_header.php";
                         Welcome to Admin
                         <small>Authors</small>
                     </h1>
+                    <?php
+                    if (isset($_GET['source'])) {
+                        $source = $_GET['source'];
+                    }else{
+                        $source='';
+                    }
+                    switch ($source) {
+                        case'1';
+                            echo "nice1";
+                            break;
 
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Author</th>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Status</th>
-                            <th>Image</th>
-                            <th>Tags</th>
-                            <th>Comments</th>
-                            <th>Date</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                        $query = "SELECT * FROM post";
-                        $select_post = mysqli_query($conn, $query);
-                        while ($row = mysqli_fetch_assoc($select_post)) {
-                            $post_id = (int)$row['post_id'];
-                            $post_author = $row['post_author'];
-                            $post_title = $row['post_title'];
-                            $post_category = $row['post_category_id'];
-                            $post_status = $row['post_status'];
-                            $post_image = $row['post_image'];
-                            $post_tags = $row['post_tags'];
-                            $post_comments = $row['post_comm_count'];
-                            $post_date = $row['post_date'];
+                        case'2';
+                            echo "nice2";
+                            break;
+
+                        case'3';
+                            echo "nice3";
+                            break;
+
+                        default:
+                            include "includes/view_all_posts.php";
+                            break;
+
+                    }
 
 
-                            echo "<tr>";
-                            echo "<td>{$post_id}</td>";
-                            echo "<td>{$post_author}</td>";
-                            echo "<td>{$post_title}</td>";
-                            echo "<td>{$post_category}</td>";
-                            echo "<td>{$post_status}</td>";
-                            echo "<td><img class='img-responsive' src='../$post_image'></td>";
-                            echo "<td>{$post_tags}</td>";
-                            echo "<td>{$post_comments}</td>";
-                            echo "<td>{$post_date}</td>";
-                            echo "<td><a href='categories.php?delete={$post_id}'>Delete</a></td>";
-                            echo "<td><a href='categories.php?edit={$post_id}'>Edit</a></td>";
-                            echo "</tr>";
-                        }
-
-                        ?>
-
-                        </tbody>
-
-                    </table>
-
-
-
-
-                    </div>
+                    ?>
 
                 </div>
-                <!-- /.row -->
 
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
 
         </div>
-        <!-- /#page-wrapper -->
-        <?php
-        include_once "includes/admin_footer.php";
-        ?>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+    <?php
+    include_once "includes/admin_footer.php";
+    ?>
