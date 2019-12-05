e1601da9287e78084782d81eda8ac256aa67b5f6
Angela Kuznetsova <solival@gmail.com>
2019-12-05 21:31:17 +1300

    section15.5

--- post.php
+++ post.php
@@ -16,7 +16,7 @@ include_once "includes/navigation.php";
         <div class="col-md-8">
             <?php
 
-            if(isset($_GET['p_id'])){
+            if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
             }
 
@@ -24,7 +24,7 @@ include_once "includes/navigation.php";
 
             $all_post = mysqli_query($conn, $query);
 
-            while($row = mysqli_fetch_assoc($all_post)){
+            while ($row = mysqli_fetch_assoc($all_post)) {
                 $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
@@ -49,14 +49,24 @@ include_once "includes/navigation.php";
                 <img class="img-responsive" src="./images/<?php echo $post_image ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn btn-primary" href="#">Read More <span
+                            class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
 
                 <?php
             }
-            if(isset($_POST['create_comm'])){
+            if (isset($_POST['create_comm'])) {
+                $the_post_id = $_GET['p_id'];
+                $comm_author = $_POST['comm_author'];
+                $comm_email = $_POST['comm_email'];
+                $comm_content = $_POST['comm_content'];
+
+                $query = "INSERT INTO comment (comm_post_id, comm_author, comm_email, comm_content, comm_date) VALUES ('{$the_post_id}', '{$comm_author}', '{$comm_email}', '{$comm_content}', now()) ";
+                $create_comm_query = mysqli_query($conn, $query);
+
+                confirm($create_comm_query);
 
             }
 
@@ -94,7 +104,9 @@ include_once "includes/navigation.php";
                     <h4 class="media-heading">Start Bootstrap
                         <small>August 25, 2014 at 9:30 PM</small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+                    vulputate fringilla. Donec lacinia congue felis in faucibus.
                 </div>
             </div>
 
@@ -107,7 +119,9 @@ include_once "includes/navigation.php";
                     <h4 class="media-heading">Start Bootstrap
                         <small>August 25, 2014 at 9:30 PM</small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+                    vulputate fringilla. Donec lacinia congue felis in faucibus.
                     <!-- Nested Comment -->
                     <div class="media">
                         <a class="pull-left" href="#">
@@ -117,14 +131,14 @@ include_once "includes/navigation.php";
                             <h4 class="media-heading">Nested Start Bootstrap
                                 <small>August 25, 2014 at 9:30 PM</small>
                             </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
+                            commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
+                            condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                         </div>
                     </div>
                     <!-- End Nested Comment -->
                 </div>
             </div>
-
-
 
 
         </div>
