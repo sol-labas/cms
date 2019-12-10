426632c3d925538e6abc90b692781c7921be341c
Angela Kuznetsova <solival@gmail.com>
2019-12-10 22:20:38 +1300

    section16.3

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -2,16 +2,13 @@
     <thead>
     <tr>
         <th>ID</th>
-        <th>Author</th>
-        <th>Comment</th>
+        <th>Username</th>
+        <th>First_name</th>
+        <th>Last_name</th>
         <th>Email</th>
-        <th>Status</th>
-        <th>In Response to</th>
+        <th>User_Image</th>
+        <th>Role</th>
         <th>Date</th>
-        <th>Approve</th>
-        <th>Unapproved</th>
-        <th>Delete</th>
-
     </tr>
     </thead>
     <tbody>
