815a276da42eac3956390d022cf5f9913c7467b5
Angela Kuznetsova <solival@gmail.com>
2019-12-12 23:24:46 +1300

    section16.13

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,52 +1,76 @@
 <?php
-if(isset($_GET['edit_user'])){
+if(isset($_GET['edit_user'])) {
+    $the_user_id = $_GET['edit_user'];
 
+    $query = "SELECT * FROM user WHERE user_id= {$the_user_id}";
+    $select_users = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_assoc($select_users)) {
+        $user_id = (int)$row['user_id'];
+        $username = $row['username'];
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+        $email = $row['email'];
+        $user_image = $row['user_image'];
+        $role = $row['role'];
+        $password = $row['password'];
+    }
 }
 
-if(isset($_POST['edit_user'])){
+//if(isset($_POST['edit_user'])){
     //$user_id = $_POST['user_id'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $role = $_POST['role'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $user_image_temp = $_FILES['user_image']['tmp_name'];
-    $user_image = $_POST['user_image'];
+    //$first_name = $_POST['first_name'];
+    //$last_name = $_POST['last_name'];
+    //$role = $_POST['role'];
+    //$username = $_POST['username'];
+    //$email = $_POST['email'];
+    //$password = $_POST['password'];
+    //$user_image_temp = $_FILES['user_image']['tmp_name'];
+    //$user_image = $_POST['user_image'];
     // $post_date = date('d-m-y');
 
     //move_uploaded_file($user_image_temp, "../images/$user_image");
 
-    $query = "INSERT INTO user (username, password, first_name, last_name, email, user_image, role ) VALUES ('{$username}', '{$password}', '{$first_name}', '{$last_name}', '{$email}', '{$user_image}', '{$role}')";
-    $create_user_query = mysqli_query($conn, $query);
+   // $query = "INSERT INTO user (username, password, first_name, last_name, email, user_image, role ) VALUES ('{$username}', '{$password}', '{$first_name}', '{$last_name}', '{$email}', '{$user_image}', '{$role}')";
+   // $create_user_query = mysqli_query($conn, $query);
 
-    confirm($create_user_query);
-}
+   // confirm($create_user_query);
+//}
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="first_name">First Name</label>
-        <input type="text" class="form-control" name="first_name">
+        <input type="text" value="<?php echo $first_name; ?>" class="form-control" name="first_name">
     </div>
 
     <div class="form-group">
         <label for="last_name">Last Name</label>
-        <input type="text" class="form-control" name="last_name">
+        <input type="text" value="<?php echo $last_name; ?>"class="form-control" name="last_name">
     </div>
 
     <div class="form-group">
         <label for="role">Role</label>
         <br>
         <select name="role" id="role">
-            <option value="admin">Admin</option>
-            <option value="subscriber">Subscriber</option>
+            <?php
+
+            $query = "SELECT * FROM user ";
+            $select_cat = mysqli_query($conn, $query);
+
+            confirm($select_cat);
+            while ($row = mysqli_fetch_assoc($select_cat)) {
+                $user_id = $row['user_id'];
+                $role = $row['role'];
+
+                echo "<option value='{$user_id}'>{$role}</option>";
+            }
+            ?>
         </select>
     </div>
 
     <div class="form-group">
         <label for="username">Username</label>
-        <input type="text" class="form-control" name="username">
+        <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
     </div>
 
     <div class="form-group">
@@ -56,12 +80,12 @@ if(isset($_POST['edit_user'])){
 
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" class="form-control" name="email">
+        <input type="email" value="<?php echo $email; ?>" class="form-control" name="email">
     </div>
 
     <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" name="password">
+        <input type="password" value="<?php echo $password; ?>" class="form-control" name="password">
     </div>
 
     <div class="form-group">
@@ -73,16 +97,4 @@ if(isset($_POST['edit_user'])){
 </form>
 
 
-<?php
 
-$query = "SELECT * FROM user ";
-$select_cat = mysqli_query($conn, $query);
-
-confirm($select_cat);
-while ($row = mysqli_fetch_assoc($select_cat)) {
-    $user_id = $row['user_id'];
-    $role = $row['role'];
-
-    echo "<option value='{$user_id}'>{$role}</option>";
-}
-?>
