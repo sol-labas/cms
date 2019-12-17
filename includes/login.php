beb7d64a9061e9d5eb9f693bc8b1666f59d97db5
Angela Kuznetsova <solival@gmail.com>
2019-12-17 20:01:21 +1300

    section17.3

--- includes/login.php
+++ includes/login.php
@@ -1,8 +1,21 @@
 <?php include_once "db.php";
 if(isset($_POST['login'])){
-echo $username = $_POST['username'];
-echo $password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
+    $username= mysqli_real_escape_string($conn, $username);
+    $password= mysqli_real_escape_string($conn, $password);
+
+    $query = "SELECT * FROM user WHERE username = '{$username}'";
+    $select_user = mysqli_query($conn, $query);
+    if(!$select_user){
+        die("QUERY FAILED" . mysqli_error($conn));
+    }
+
+    while($row = mysqli_fetch_assoc($select_user)){
+        echo  $db_id = $row['user_id'];
+
+    }
 
 }
 
