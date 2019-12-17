bba75a4d2c1cbe49504c06b09984684b47a8a419
Angela Kuznetsova <solival@gmail.com>
2019-12-17 20:23:22 +1300

    section17.4

--- includes/login.php
+++ includes/login.php
@@ -1,27 +1,34 @@
 <?php include_once "db.php";
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $username= mysqli_real_escape_string($conn, $username);
-    $password= mysqli_real_escape_string($conn, $password);
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
 
     $query = "SELECT * FROM user WHERE username = '{$username}'";
     $select_user = mysqli_query($conn, $query);
-    if(!$select_user){
+    if (!$select_user) {
         die("QUERY FAILED" . mysqli_error($conn));
     }
 
-    while($row = mysqli_fetch_assoc($select_user)){
-        echo  $db_id = $row['user_id'];
-
+    while ($row = mysqli_fetch_assoc($select_user)) {
+        $db_id = $row['user_id'];
+        $db_username = $row['username'];
+        $db_first_name = $row['first_name'];
+        $db_last_name = $row['last_name'];
+        $db_role = $row['role'];
+        $db_password = $row['password'];
     }
 
+    if ($username !== $db_username && $password !== $db_password) {
+        header("Location: ../index.php");
+    } elseif ($username == $db_username && $password == $db_password) {
+        header("Location: ../admin");
+    } else {
+        header("Location: ../index.php");
+    }
 }
-
-
-
-
 
 
 ?>
