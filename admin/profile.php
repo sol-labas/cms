e44eddb02c24889ecefbdb78098c1a57dcb0ad2e
Angela Kuznetsova <solival@gmail.com>
2019-12-18 21:59:24 +1300

    section18.2

--- admin/profile.php
+++ admin/profile.php
@@ -1,6 +1,23 @@
 <?php
 include_once "includes/admin_header.php";
-if(isset($_SESSION['username'])){
+if (isset($_SESSION['username'])) {
+
+    $username = $_SESSION['username'];
+    $query = "SELECT * FROM user WHERE username = '{$username}'";
+    $select_profile = mysqli_query($conn, $query);
+
+    while ($row = mysqli_fetch_array($select_profile)) {
+        $user_id = (int)$row['user_id'];
+        $username = $row['username'];
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+        $email = $row['email'];
+        $user_image = $row['user_image'];
+        $role = $row['role'];
+        $password = $row['password'];
+
+    }
+
 
 }
 
@@ -21,62 +38,65 @@ if(isset($_SESSION['username'])){
                     <h1 class="page-header">
                         Welcome to Admin
                         <small>Authors</small>
-                        <form action="" method="post" enctype="multipart/form-data">
-                            <div class="form-group">
-                                <label for="first_name">First Name</label>
-                                <input type="text" value="<?php echo $first_name; ?>" class="form-control" name="first_name">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" value="<?php echo $last_name; ?>" class="form-control" name="last_name">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="role">Role</label>
-                                <br>
-                                <select name="role" id="role">
-
-                                    <option value='admin'><?php echo strtoupper($role); ?></option>
-                                    <?php
-                                    if ($role == 'admin') {
-                                        echo "<option value='subscriber'>subscriber</option>>";
-                                    } else {
-
-                                        echo "<option value='admin'>admin</option>>";
-                                    }
-                                    ?>
-
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="user_image">Avatar</label>
-                                <input type="file" name="user_image">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" value="<?php echo $email; ?>" class="form-control" name="email">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" value="<?php echo $password; ?>" class="form-control" name="password">
-                            </div>
-
-                            <div class="form-group">
-                                <input class="btn btn-primary" type="submit" name="edit_user" value="Edit User">
-                            </div>
-
-
-                        </form>
                     </h1>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" value="<?php echo $first_name; ?>" class="form-control"
+                                   name="first_name">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" value="<?php echo $last_name; ?>" class="form-control" name="last_name">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="role">Role</label>
+                            <br>
+                            <select name="role" id="role">
+
+                                <option value='admin'><?php echo strtoupper($role); ?></option>
+                                <?php
+                                if ($role == 'admin') {
+                                    echo "<option value='subscriber'>subscriber</option>>";
+                                } else {
+
+                                    echo "<option value='admin'>admin</option>>";
+                                }
+                                ?>
+
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="user_image">Avatar</label>
+                            <input type="file" name="user_image">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" value="<?php echo $email; ?>" class="form-control" name="email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" value="<?php echo $password; ?>" class="form-control"
+                                   name="password">
+                        </div>
+
+                        <div class="form-group">
+                            <input class="btn btn-primary" type="submit" name="edit_user" value="Update Profile">
+                        </div>
+
+
+                    </form>
+
 
                 </div>
 
