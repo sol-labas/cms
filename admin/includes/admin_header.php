e75d17126ce6d2a205009b93a1fad133287e64ff
Angela Kuznetsova <solival@gmail.com>
2020-01-21 22:21:12 +1300

    section 23.4

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -40,6 +40,9 @@ include "function.php";
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link href="css/style.css" rel="stylesheet">
+
     <script src="js/jquery.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
